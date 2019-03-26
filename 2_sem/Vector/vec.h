8b5baf3766f3901b8f936243ec0002667878efec
HiXY <fear1396@mail.ru>
2019-03-27 00:42:58 +0300

    Fixed errors in templates and variables. (Now its compilling).

--- 2_sem/Vector/vec.h
+++ 2_sem/Vector/vec.h
@@ -1,4 +1,5 @@
 #include "alloc.h"
+#include <memory>
 
 template <typename T, typename Allocator = MyAlloc <T> >
 class MyVector
@@ -13,8 +14,8 @@ class MyVector
         typedef std::size_t size_type;
 
 		explicit MyVector (const allocator_type &alloc) noexcept;
-		MyVector (size_type count, const T &value, const allocator_type &alloc = Allocator ());
-		MyVector (size_type count, const allocator_type &alloc = Allocator());
+		MyVector (size_type count, const T &value, const allocator_type &alloc);
+		MyVector (size_type count, const allocator_type &alloc);
 		MyVector (const MyVector &other, const allocator_type &alloc);
 		MyVector (MyVector &&other, const allocator_type &alloc) noexcept;
 		~MyVector () noexcept;
@@ -69,97 +70,97 @@ class MyVector
 		}
 };
 
-template <typename T>
-MyVector <T>::MyVector (const typename MyVector <T>::allocator_type &alloc) noexcept : Allocator (alloc),
+template <typename T, typename Allocator>
+MyVector <T, Allocator>::MyVector (const typename MyVector <T, Allocator>::allocator_type &alloc) noexcept : Allocator (alloc),
 begin_ (nullptr), end_ (nullptr), end_of_storage_ (nullptr)
 {}
-template <typename T>
-MyVector <T>::MyVector (typename MyVector <T>::size_type count, const T &value, const typename MyVector <T>::allocator_type &alloc = Allocator ()) :
+template <typename T, typename Allocator>
+MyVector <T, Allocator>::MyVector (typename MyVector <T, Allocator>::size_type count, const T &value, const typename MyVector <T, Allocator>::allocator_type &alloc) :
 Allocator (alloc), begin_ (allocate (count)), end_ (begin_ + count), end_of_storage_ (end_)
 {
-	std::uninitialized_fill_n <pointer, typename MyVector <T>::size_type>(begin_, count, value);
+	std::uninitialized_fill_n <pointer, typename MyVector <T, Allocator>::size_type>(begin_, count, value);
 }
-template <typename T>
-MyVector <T>::MyVector (typename MyVector <T>::size_type count, const typename MyVector <T>::allocator_type &alloc = Allocator ()) :
+template <typename T, typename Allocator>
+MyVector <T, Allocator>::MyVector (typename MyVector <T, Allocator>::size_type count, const typename MyVector <T, Allocator>::allocator_type &alloc) :
 Allocator (alloc), begin_ (allocate (count)), end_ (begin_ + count), end_of_storage_ (end_)
 {}
-template <typename T>
-MyVector <T>::MyVector (const MyVector <T> &other, const typename MyVector <T>::allocator_type &alloc) : Allocator (alloc),
+template <typename T, typename Allocator>
+MyVector <T, Allocator>::MyVector (const MyVector <T, Allocator> &other, const typename MyVector <T, Allocator>::allocator_type &alloc) : Allocator (alloc),
 begin_ (allocate (other.size ())), end_ (begin_ + other.size ()), end_of_storage_(end_)
 {
 	memcpy (begin_, other.begin_, other.size () * sizeof (pointer));
 }
-template <typename T>
-MyVector <T>::MyVector (MyVector &&other, const typename MyVector <T>::allocator_type &alloc) noexcept : Allocator (alloc),
+template <typename T, typename Allocator>
+MyVector <T, Allocator>::MyVector (MyVector &&other, const typename MyVector <T, Allocator>::allocator_type &alloc) noexcept : Allocator (alloc),
 begin_ (other.begin_), end_ (other.end_), end_of_storage_ (other.end_of_storage_)
 {
 	other.begin_ = nullptr;
 	other.end_ = nullptr;
 	other.end_of_storage_ = nullptr;
 }
-template <typename T>
-MyVector <T>::~MyVector () noexcept
+template <typename T, typename Allocator>
+MyVector <T, Allocator>::~MyVector () noexcept
 {
-	deallocate ()
+	deallocate ();
 }
-template <typename T>
-typename MyVector <T>::pointer MyVector <T>::begin () noexcept {return begin_;}
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::pointer MyVector <T, Allocator>::begin () noexcept {return begin_;}
 
-template <typename T>
-typename MyVector <T>::const_pointer MyVector <T>::begin () const noexcept {return begin_;}
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::const_pointer MyVector <T, Allocator>::begin () const noexcept {return begin_;}
 
-template <typename T>
-typename MyVector <T>::pointer MyVector <T>::end () noexcept {return end_;}
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::pointer MyVector <T, Allocator>::end () noexcept {return end_;}
 
-template <typename T>
-typename MyVector <T>::const_pointer MyVector <T>::end () const noexcept {return end_;}
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::const_pointer MyVector <T, Allocator>::end () const noexcept {return end_;}
 
-template <typename T>
-typename MyVector <T>::reference MyVector <T>::at (typename MyVector <T>::size_type index)
-{
-	if (index >= size ())
-		throw std::out_of_range ("Access to element in vector after end\n");
-	return begin_[index];
-}
-template <typename T>
-typename MyVector <T>::const_reference MyVector <T>::at (typename MyVector <T>::size_type index) const
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::reference MyVector <T, Allocator>::at (typename MyVector <T, Allocator>::size_type index)
 {
 	if (index >= size ())
 		throw std::out_of_range ("Access to element in vector after end\n");
 	return begin_[index];
 }
-template <typename T>
-typename MyVector <T>::reference MyVector <T>::operator [] (typename MyVector <T>::size_type index) {return begin_[index];}
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::const_reference MyVector <T, Allocator>::at (typename MyVector <T, Allocator>::size_type index) const
+{
+	if (index >= size ())
+		throw std::out_of_range ("Access to element in vector after end\n");
+	return begin_[index];
+}
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::reference MyVector <T, Allocator>::operator [] (typename MyVector <T, Allocator>::size_type index) {return begin_[index];}
 
-template <typename T>
-typename MyVector <T>::const_reference MyVector <T>::operator [] (typename MyVector <T>::size_type index) {return begin_[index];}
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::const_reference MyVector <T, Allocator>::operator [] (typename MyVector <T, Allocator>::size_type index) const {return begin_[index];}
 
-template <typename T>
-typename MyVector <T>::reference MyVector <T>::front () noexcept {return begin_;}
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::reference MyVector <T, Allocator>::front () noexcept {return begin_;}
 
-template <typename T>
-typename MyVector <T>::const_reference MyVector <T>::front () const noexcept {return begin_;}
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::const_reference MyVector <T, Allocator>::front () const noexcept {return begin_;}
 
-template <typename T>
-typename MyVector <T>::reference MyVector <T>::back () noexcept {return end_ - 1;}
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::reference MyVector <T, Allocator>::back () noexcept {return end_ - 1;}
 
-template <typename T>
-typename MyVector <T>::const_reference MyVector <T>::back () const noexcept {return end_ - 1;}
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::const_reference MyVector <T, Allocator>::back () const noexcept {return end_ - 1;}
 
-template <typename T>
-typename MyVector <T>::size_type MyVector <T>::size () const noexcept { return end_ - begin_; }
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::size_type MyVector <T, Allocator>::size () const noexcept { return end_ - begin_; }
 
-template <typename T>
-typename MyVector <T>::size_type MyVector <T>::max_size () const noexcept { return Allocator::max_size (); }
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::size_type MyVector <T, Allocator>::max_size () const noexcept { return Allocator::max_size (); }
 
-template <typename T>
-typename MyVector <T>::size_type MyVector <T>::capacity() const noexcept { return end_of_storage_ - begin_; }
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::size_type MyVector <T, Allocator>::capacity() const noexcept { return end_of_storage_ - begin_; }
 
-template <typename T>
-bool MyVector <T>::empty () const noexcept { return begin_ == end_; }
+template <typename T, typename Allocator>
+bool MyVector <T, Allocator>::empty () const noexcept { return begin_ == end_; }
 
-template <typename T>
-void MyVector <T>::assign (typename MyVector <T>::size_type count, const T &value)
+template <typename T, typename Allocator>
+void MyVector <T, Allocator>::assign (typename MyVector <T, Allocator>::size_type count, const T &value)
 {
 	if (count > capacity ())
 	{
@@ -169,17 +170,17 @@ void MyVector <T>::assign (typename MyVector <T>::size_type count, const T &valu
 		end_of_storage_ = begin_ + count;
 	}
 	end_ = begin_ + count;
-	std::uninitialized_fill_n <pointer, typename MyVector <T>::size_type> (begin_, count, value);
+	std::uninitialized_fill_n <pointer, typename MyVector <T, Allocator>::size_type> (begin_, count, value);
 }
-template <typename T>
-typename MyVector <T>::pointer MyVector <T>::insert (typename MyVector <T>::const_pointer pos, const T &value)
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::pointer MyVector <T, Allocator>::insert (typename MyVector <T, Allocator>::const_pointer pos, const T &value)
 {
 	if (end_ == end_of_storage_)
 	{
-		typename MyVector <T>::size_type index = pos - begin_;
+		typename MyVector <T, Allocator>::size_type index = pos - begin_;
 		pointer new_storage = allocate (size () + 1);
 		memcpy (new_storage, begin_, index * sizeof (value_type));
-		memcpy (new_storage + index + 1, _begin + index, (size () - index) * sizeof(value_type));
+		memcpy (new_storage + index + 1, begin_ + index, (size () - index) * sizeof(value_type));
 		deallocate ();
 		end_ = new_storage + size () + 1;
 		begin_ = new_storage;
@@ -189,20 +190,20 @@ typename MyVector <T>::pointer MyVector <T>::insert (typename MyVector <T>::cons
 	else
 	{
 		memmove (const_cast <pointer> (pos) + 1, pos, (end_ - pos) * sizeof (value_type));
-		_end++;
+		end_++;
 	}
 	*const_cast <pointer> (pos) = value;
 	return const_cast <pointer> (pos);
 }
-template <typename T>
-typename MyVector <T>::pointer MyVector <T>::insert (typename MyVector <T>::const_pointer pos, T &&value)
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::pointer MyVector <T, Allocator>::insert (typename MyVector <T, Allocator>::const_pointer pos, T &&value)
 {
 	if (end_ == end_of_storage_)
 	{
-		typename MyVector <T>::size_type index = pos - begin_;
+		typename MyVector <T, Allocator>::size_type index = pos - begin_;
 		pointer new_storage = allocate (size () + 1);
 		memcpy (new_storage, begin_, index * sizeof (value_type));
-		memcpy (new_storage + index + 1, _begin + index, (size () - index) * sizeof(value_type));
+		memcpy (new_storage + index + 1, begin_ + index, (size () - index) * sizeof(value_type));
 		deallocate ();
 		end_ = new_storage + size () + 1;
 		begin_ = new_storage;
@@ -212,28 +213,28 @@ typename MyVector <T>::pointer MyVector <T>::insert (typename MyVector <T>::cons
 	else
 	{
 		memmove (const_cast <pointer> (pos) + 1, pos, (end_ - pos) * sizeof (value_type));
-		_end++;
+		end_++;
 	}
 	*const_cast <pointer> (pos) = std::move (value);
 	return const_cast <pointer> (pos);
 }
-template <typename T>
-typename MyVector <T>::pointer MyVector <T>::erase (typename MyVector <T>::const_pointer pos)
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::pointer MyVector <T, Allocator>::erase (typename MyVector <T, Allocator>::const_pointer pos)
 {
 	end_--;
 	memmove (const_cast <pointer> (pos), pos + 1, (end_ - pos) * sizeof (value_type));
 	return const_cast <pointer> (pos);
 }
-template <typename T>
-typename MyVector <T>::pointer MyVector <T>::erase (typename MyVector <T>::const_pointer first, typename MyVector <T>::const_pointer last)
+template <typename T, typename Allocator>
+typename MyVector <T, Allocator>::pointer MyVector <T, Allocator>::erase (typename MyVector <T, Allocator>::const_pointer first, typename MyVector <T, Allocator>::const_pointer last)
 {
-	typename MyVector <T>::size_type count = last - first;
-	_end -= count;
-	memmove (const_cast <pointer> (first), first + count, (end_ - first) * sizeof (value_type));
+	typename MyVector <T, Allocator>::size_type count = last - first;
+	end_-= count;
+	memmove (const_cast <pointer> (first), first + count, (end_ - first) * sizeof (value_type));    /// std copy
 	return const_cast <pointer> (first);
 }
-template <typename T>
-void MyVector <T>::push_back (const T &value)
+template <typename T, typename Allocator>
+void MyVector <T, Allocator>::push_back (const T &value)
 {
 	*end_ = insert (end_, value);
 	/*
@@ -250,8 +251,8 @@ void MyVector <T>::push_back (const T &value)
 	end_++;
 	*/
 }
-template <typename T>
-void MyVector <T>::push_back (T &&value)
+template <typename T, typename Allocator>
+void MyVector <T, Allocator>::push_back (T &&value)
 {
 	*end_ = insert (end_, value);
 	/*
@@ -268,15 +269,15 @@ void MyVector <T>::push_back (T &&value)
 	end_++;
 	*/
 }
-template <typename T>
-void MyVector <T>::pop_back () {end_--;}
+template <typename T, typename Allocator>
+void MyVector <T, Allocator>::pop_back () {end_--;}
 
-template <typename T>
-void MyVector <T>::resize (typename MyVector <T>::size_type count)
+template <typename T, typename Allocator>
+void MyVector <T, Allocator>::resize (typename MyVector <T, Allocator>::size_type count)
 {
 	if (capacity () < count)
 	{
-		typename MyVector <T>::size_type new_size = size () + count;
+		typename MyVector <T, Allocator>::size_type new_size = size () + count;
 		pointer new_storage = allocate (new_size);
 		memcpy (new_storage, begin_, size() * sizeof(value_type));
 		deallocate ();
@@ -285,8 +286,8 @@ void MyVector <T>::resize (typename MyVector <T>::size_type count)
 	}
 	end_ = begin_ + count;
 }
-template <typename T>
-void MyVector <T>::reserve (typename MyVector <T>::size_type new_cap)
+template <typename T, typename Allocator>
+void MyVector <T, Allocator>::reserve (typename MyVector <T, Allocator>::size_type new_cap)
 {
 	if (capacity () < new_cap)
 	{
@@ -299,5 +300,5 @@ void MyVector <T>::reserve (typename MyVector <T>::size_type new_cap)
 		end_of_storage_ = begin_ + new_cap;
 	}
 }
-template <typename T>
-void MyVector <T>::clear () noexcept {end_ = begin_;}
+template <typename T, typename Allocator>
+void MyVector <T, Allocator>::clear () noexcept {end_ = begin_;}
